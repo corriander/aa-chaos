e52b35fd936382abd3d6d0d1b9153cbb81a74e32
Alex Corrie <ajccode@gmail.com>
2015-10-31 14:04:13 +0000

    Added a "main" script as an entry-point.
    This is actually on its second iteration now. Originally it accepted
    credentials from a specified file or explicitly. The file location is
    now implicitly located in the XDG_CONFIG_HOME directory much like the
    database.
    
    Previous code has been commented out and will be removed at a later
    date (the ability to specify auth file on the command line may be
    reinstated).

--- aachaos/get.py
+++ aachaos/get.py
@@ -3,6 +3,7 @@
 Acts as an adapter to the (at time of writing: changing) chaos API.
 """
 
+import os
 import requests
 import xml.etree.ElementTree as ET
 from collections import namedtuple
@@ -10,6 +11,13 @@ from datetime import datetime
 
 # How to derive this in ElementTree from the source?
 URL_CHAOS = 'https://chaos.aa.net.uk/'
+
+PATH_CFG_DEFAULT = os.path.join(
+    os.getenv('HOME'),
+    '.config',
+    os.path.basename(os.path.dirname(__file__))
+)
+PATH_CFG = os.getenv('XDG_CONFIG_HOME', PATH_CFG_DEFAULT)
 
 Quota = namedtuple('Quota', 'tstamp, rem, tot')
 
@@ -60,3 +68,50 @@ class LineInfo(object):
         q_left = int(d['quota-left']) # bytes
         q_tot = int(d['quota-monthly']) # bytes
         self._quota = Quota(q_tstamp, q_left, q_tot)
+
+    @classmethod
+    def credentials(cls, obj):
+        if not isinstance(obj, Credentials):
+            raise TypeError("Credentials object must be provided.")
+        else:
+            return cls(obj.user, obj.passwd)
+
+
+class Credentials(object):
+    """Encapsulate authorisation/credentials functionality.
+
+    Credentials are either provided at runtime or retrieved from a
+    plain text file with a "secure" (600) local file (configured
+    separately). By default, instances assume the latter unless
+    credentials are explicitly provided.
+    """
+
+    class FileNotPresent(RuntimeError): pass
+    class FileNotSecure(RuntimeError): pass
+
+    auth_path = os.path.join(PATH_CFG, 'auth')
+
+    def __init__(self, user=None, passwd=None):
+        if self._userpasswd_provided(user, passwd):
+            self.user, self.passwd = user, passwd
+        else:
+            self.user, self.passwd = self.retrieve()
+
+    @staticmethod
+    def _userpasswd_provided(user, passwd):
+        return user is not None and passwd is not None
+
+    def _auth_file_present(self):
+        return os.path.isfile(self.auth_path)
+
+    def _permissions_ok(self):
+        return oct(os.stat(self.auth_path).st_mode & 0o777) == '0o600'
+
+    def retrieve(self):
+        if not self._auth_file_present():
+            raise self.FileNotPresent
+        elif not self._permissions_ok():
+            raise self.FileNotSecure
+        else:
+            with open(self.auth_path, 'r') as f:
+                return tuple(f.readline().strip().split(':'))
---
+++ aachaos/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import os
+import argparse
+from datetime import datetime
+
+import get, store
+
+T_ELAPSED_MIN = 10800 # s, minimum elapsed time.
+
+def main(user, passwd):
+    """Download quota and store locally.
+
+    Takes a single argument which may be a file path or a 2-tuple
+    containing username and password strings; only downloads quota if
+    sufficient time has passed since the last fetch (currently 3
+    hours).
+    """
+
+    # Pre-requisites
+    ## Database check (TODO: store this in a static file in XDG_DATA)
+    db = store.DB()
+    cursor = db.execute("SELECT max(timestamp) FROM quota_history")
+    max_tstamp = cursor.fetchone()[0]
+    try:
+        t_latest = db.dbdt_to_pydt(max_tstamp)
+        t_elapsed = datetime.now() - t_latest
+        if t_elapsed.total_seconds() < T_ELAPSED_MIN:
+            return
+    except TypeError as err:
+        if 'Must be str, not None' in str(err):
+            pass
+
+    info = get.LineInfo(user, passwd)
+    db.insert_quota(*info.quota)
+    db.commit()
+    return 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Fetch internet usage/quota."
+    )
+    parser.add_argument('--user', dest='user')
+    parser.add_argument('--pass', dest='passwd')
+    #parser.add_argument('--file', dest='fpath')
+    args = parser.parse_args()
+
+    #fpath = args.fpath
+    #user_passwd = (args.user, args.passwd)
+    auth = get.Credentials(args.user, args.passwd)
+    #if fpath is not None:
+    #    try:
+    #        #if (oct(os.stat(fpath).st_mode & 0o777) != '600'):
+    #        #    raise Exception("Credential file needs 600 perms")
+    #        #else:
+    #        with open(fpath, 'r') as f:
+    #            auth = tuple(f.readline().strip().split(':'))
+    #    except:
+    #        raise
+
+    #elif all(arg is not None for arg in user_passwd):
+    #    auth = user_passwd
+    #else:
+    #    raise Exception
+
+    #main(*auth)
+    main(auth.user, auth.passwd)
