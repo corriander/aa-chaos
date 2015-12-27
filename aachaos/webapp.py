a8b78da6a69e4c85d631aa8f698e748eda4ce8d0
Alex Corrie <ajccode@gmail.com>
2015-12-27 11:33:48 +0000

    Use a StaticFileHandler
    Although a RequestHandler subclass works, it's the wrong tool for the
    job of serving a (quasi-) static image. Browsers cache this
    aggressively, preventing the image updating when it changes. For this
    purpose, a StaticFileHandler subclass allows control over the caching
    HTTP headers which instruct a browser to re-pull the image.

--- aachaos/webapp.py
+++ aachaos/webapp.py
@@ -1,18 +1,27 @@
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        # TODO: tsk tsk, hardcoding.
-        path = '/tmp/aachaos_usage_monitor.svg'
-        self.render(path)
+
+# TODO: fix hardcoding / filename
+FIG_PATH = '/tmp/aachaos_usage_monitor.svg'
+TMP_PATH = '/tmp/aachaos_usage_monitor.tmp.svg'
+
+
+class QuasiStaticHandler(tornado.web.StaticFileHandler):
+    def set_extra_headers(self, path):
+        self.set_header('Cache-Control',
+                        'no-store, no-cache, must-revalidate')
+
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        # The fig could be mapped to e.g. server:port/netquota with
+        # r'/netquota()'
+        (r'/()', QuasiStaticHandler, {'path': FIG_PATH}),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(9988)
+    app.listen(8889)
     tornado.ioloop.IOLoop.current().start()
